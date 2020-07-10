3e0621201bcde1e71cd0f02a31f738e00ff3c1d4
Gaurang <gaurang.p@shaligraminfotech.com>
2020-07-10 14:59:59 +0530

    seeder added

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'id' => 10,
+            'name' => 'Admin',
+            'role' => 1,
+            'email' => 'admin@gmail.com',
+            'password' => md5('123456'),
+        ]);
+    }
+}
